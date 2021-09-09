d4fc2d33eccdee85929874e05f51ebd8b913d492
Ross Turner <51308689+rossjturner@users.noreply.github.com>
2021-09-09 15:50:46 +1000

    version 1.0.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='seismic_attributes',
-    version='1.0.3',
+    version='1.0.3-1',
     author = "Ross Turner",
     author_email = "turner.rj@icloud.com",
     description = ("An ObsPy library for event detection and seismic attribute calculation: preparing waveforms for automated analysis."),
